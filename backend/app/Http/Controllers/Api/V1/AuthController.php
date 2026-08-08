d32008f5e0d7adae9e2e9fc6281742858c23cd3d
Eyyu <eyufeyisa7@gmail.com>
2026-08-08 15:29:44 +0300

    authcontroller-method register finished tested with postman

--- backend/app/Http/Controllers/Api/V1/AuthController.php
+++ backend/app/Http/Controllers/Api/V1/AuthController.php
@@ -4,14 +4,33 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(): JsonResponse
+    public function register(request $request): JsonResponse
     {
+        $validated = $request->validate([
+            'first_name'=>['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:users,email'],
+            'phone' => ['nullable', 'string', 'max:30'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+        $user = User::create([
+            'first_name'=>$validated['first_name'],
+            'last_name'=>$validated['last_name'],
+            'email'=>$validated['email'],
+            'phone'=>$validated['phone'] ?? null , 
+            'password'=>Hash::make($validated['password'])
+        ]);
+
         return response()->json([
-            'message' => 'Endpoint skeleton only. Implementation pending.',
-        ], 501);
+            'message' => 'Registration Successful',
+            'user'=> $user,
+        ], 201);
     }
 
     public function login(): JsonResponse
