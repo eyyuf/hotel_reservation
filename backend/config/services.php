f2f555c38f6201843d391a5f5b3ce1a0956c8de9
Eyyu <eyufeyisa7@gmail.com>
2026-09-17 18:52:53 +0300

    chappa fixed

--- backend/config/services.php
+++ backend/config/services.php
@@ -6,12 +6,6 @@ return [
     |--------------------------------------------------------------------------
     | Third Party Services
     |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
     */
 
     'postmark' => [
@@ -39,7 +33,10 @@ return [
         'secret_key' => env('CHAPA_SECRET_KEY'),
         'base_url' => env('CHAPA_BASE_URL', 'https://api.chapa.co/v1'),
         'mode' => env('CHAPA_MODE', 'test'),
-        'frontend_url' => env('FRONTEND_URL', 'http://localhost:5173'),
+        'frontend_url' => env(
+            'FRONTEND_URL',
+            'https://adar-hotel.vercel.app'
+        ),
     ],
 
 ];
